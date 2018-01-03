4ce9cb8dd191cd6f31cde0ff4659472241092bf0
Jacob Colborn <jcolborn@dropdeadgames.org>
2018-01-02 23:17:30 -0500

    Initial Commit
    Initial commit of the Jamuary game jam game

---
+++ Assets/scripts/playerController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : physicObjects {
+
+    public float jumpTakeOffSpeed = 7;
+    public float maxSpeed = 7;
+
+    private SpriteRenderer spriteRenderer;
+    private Animator animator;
+
+    private void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
+    }
+
+    protected override void ComputeVelocity ()
+    {
+        Vector2 move = Vector2.zero;
+
+        move.x = Input.GetAxis("Horizontal");
+
+        if (Input.GetButtonDown("Jump") && grounded)
+        {
+            velocity.y = jumpTakeOffSpeed;
+        } else if (Input.GetButtonUp("Jump")) {
+            if (velocity.y > 0)
+            {
+                velocity.y = velocity.y * 0.5f;
+            }
+        }
+
+        if (Mathf.Abs(move.x) > 0)
+        {
+            if (move.x < 0)
+            {
+                spriteRenderer.flipX = true;
+            } else if (move.x > 0) {
+                spriteRenderer.flipX = false;
+            }
+        }
+
+        animator.SetBool("grounded", grounded);
+        animator.SetFloat("velocityX", Mathf.Abs(velocity.x) / maxSpeed);
+               
+        targetVelocity = move * maxSpeed;
+	} // end of overrided ComputeVelocity
+}
