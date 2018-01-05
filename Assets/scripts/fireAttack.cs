c6172cdaba6f7b44495cb7fc57d003b20678a8ee
Jacob Colborn <jcolborn@dropdeadgames.org>
2018-01-04 22:32:38 -0500

    Added fire blasts
    Created a child and script to control fire out of the
    gun

---
+++ Assets/scripts/fireAttack.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fireAttack : MonoBehaviour {
+
+    public GameObject firePrefab;
+
+    private GameObject _fireInstance;
+    private Vector2 parentTransform;
+
+    // Update is called once per frame
+    void Update ()
+    {
+	    if (Input.GetButtonDown("Fire1"))
+        { 
+            _fireInstance = Instantiate(firePrefab, transform.parent);
+            _fireInstance.transform.parent = transform;
+        }
+        if (Input.GetButtonUp("Fire1"))
+        {
+            GameObject.Destroy(_fireInstance);
+        }
+	}
+}
